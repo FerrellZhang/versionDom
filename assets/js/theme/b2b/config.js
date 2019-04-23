ff6799ac4afd138e250be7fa96d6211ebe6edf8c
FerrellZhang <ferrell.zhang@silksoftware>
2019-04-23 11:10:58 +0800

    添加js b2b

---
+++ assets/js/theme/b2b/config.js
@@ -0,0 +1,6 @@
+//this is a config template. Create a config.js in b2b directory with this content.
+export default {
+    storeHash: "alpcmwjlu6",
+    apiRootUrl: "https://8l6t4dsd6e.execute-api.us-west-2.amazonaws.com/prod" // pro
+  // apiRootUrl: "https://fl4mq0bm40.execute-api.us-west-2.amazonaws.com/prod"
+}
